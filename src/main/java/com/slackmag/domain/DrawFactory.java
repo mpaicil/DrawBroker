99f3f94242dc366955f0422d2b60aef144d92fa7
Mauricio Paicil <mauricio.paicil@latam.com>
2017-11-30 18:21:01 -0300

    se agregan primeras funciones

---
+++ src/main/java/com/slackmag/domain/DrawFactory.java
@@ -0,0 +1,24 @@
+package com.slackmag.domain;
+
+import com.slackmag.domain.almanaque.Draw;
+import com.slackmag.domain.polla.SorteoDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+
+@Slf4j
+public class DrawFactory {
+
+    private Integer validSize;
+
+    public DrawFactory(@Value("draw.results.validSize") Integer resultSize){
+        this.validSize = resultSize;
+    }
+
+    public Draw createDraw(SorteoDTO sorteo) {
+        return null;
+    }
+
+    public Boolean hasCorrectSize(Integer incomingResultsSize) {
+        return incomingResultsSize.equals(validSize);
+    }
+}
---
+++ src/test/java/com/slackmag/domain/DrawFactoryTest.java
@@ -0,0 +1,35 @@
+package com.slackmag.domain;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class DrawFactoryTest {
+
+    @Mock
+    private
+
+    private DrawFactory drawFactory;
+    private static final Integer VALID_SIZE = 50;
+    private static final String RESULT = "3,17,35,14,25,13,12,34,28,2,17,9,3,20,19,25,9,14,35,3,10,32,5,14,11,22,13,21,32,26,18,30,8,25,35,11,29,40,38,37,30,2,36,29,7,15,30,23,6,6";
+
+    @Before
+    public void setup() {
+        drawFactory = new DrawFactory(VALID_SIZE);
+    }
+
+    @Test
+    public void verifyTheCorrectSizeOfTheResults() {
+        String[] splitResults = RESULT.split(",");
+        assertThat(drawFactory.hasCorrectSize( splitResults.length ), is(true));
+    }
+
+    @Test
+    public void verifyTheCorrectExtractionOfTheLoto(){
+
+    }
+
+}
